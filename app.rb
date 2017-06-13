96fba7ce90763a1578b295f9061745af47848492
r.kugot <stream13k@gmail.com>
2017-06-13 20:52:47 +0300

    Recreate db, add save erb,

--- app.rb
+++ app.rb
@@ -3,12 +3,29 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
+require 'nokogiri'
+require 'open-uri'
 
-set :database, "sqlite3:parses.db"
+set :database, "sqlite3:parser.db"
 
 class Product < ActiveRecord::Base
 end
 
 get '/' do
-	erb :index			
+	erb "Welcome to simple parser"			
+end
+
+get '/save' do
+	erb :save
+end
+
+post '/save' do
+	url = params[:url]
+	product = Nokogiri::HTML(open(url))
+	title = product.xpath('//*[@id="right"]/div/div[1]/div/h1/text()').text.strip
+	labels = product.xpath("//ul[contains(@class,'attribute_labels_lists')]").length - 1
+    pack = product.xpath("//span[contains(@class,'attribute_name')]")
+    price = product.xpath("//span[contains(@class,'attribute_price')]")
+    image = product.xpath("//img[contains(@id,'bigpic')]/@src")
+	erb "You save #{url}, #{title}, #{labels}"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613142441) do
+ActiveRecord::Schema.define(version: 20170613174758) do
 
   create_table "products", force: :cascade do |t|
     t.text "title"
-    t.text "pack"
-    t.text "price"
+    t.decimal "pack"
+    t.decimal "price"
     t.text "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
